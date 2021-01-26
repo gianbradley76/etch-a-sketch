1d7fbe5138079dbf0073aab0f1b1fd7a16c70b6a
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2021-01-26 18:17:10 +0800

    fixed range input. re-wrote code

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-const container = document.getElementById("container");
+const container = document.querySelector(".container");
+const reset = document.getElementById("reset-btn");
+const slider = document.getElementById("range");
 
-// Creates the grid
-function make_items(cell_num) {
+// Create grid
+function makeGrid(cell_num) {
+	container.style.gridTemplateColumns = `repeat(${cell_num}, auto)`;
+
 	let r = "";
-	for (let i = 0; i < cell_num; i++) {
-		r += '<div class="row">';
-		for (let j = 0; j < cell_num; j++) {
-			r += '<div class="cell"></div>';
-		}
-		r += "</div>";
+	for (let i = 0; i < cell_num * cell_num; i++) {
+		r += '<div class="grid-cell"></div>';
 	}
+
 	container.innerHTML = r;
 }
-make_items(16);
 
-// For changing of cell color when hovered
-const cells = document.querySelectorAll(".cell");
+// Create initial grid
+makeGrid(slider.value);
+let cells = document.querySelectorAll(".grid-cell");
+
+// Change grid size depending on slide value
+slider.onmouseup = () => {
+	makeGrid(slider.value);
+	let cells = document.querySelectorAll(".grid-cell");
+	cells.forEach((cell) => {
+		cell.addEventListener("mouseover", change_background);
+		cell.addEventListener("mouseout", change_background);
+	});
+};
 
 function change_background() {
 	this.classList.add("hovered");
 }
 
+// change color when hovered
 cells.forEach((cell) => {
 	cell.addEventListener("mouseover", change_background);
 	cell.addEventListener("mouseout", change_background);
+});
+
+// reset grid size and removes drawing
+reset.addEventListener("click", () => {
+	document.querySelectorAll(".grid-cell").forEach((cell) => {
+		cell.classList.remove("hovered");
+	});
 });
